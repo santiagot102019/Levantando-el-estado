8c0fe2f88839578635d96dcf93e4b2e3ea26fd67
santiagot102019 <santiagot102019@gmail.com>
2021-08-30 18:04:26 -0500

    1 file modified, 3 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './style.css';
-import Calculator from './Calculator.js';
 
 export default function App() {
   return (
     <div>
-      <Calculator />
+      <h1>Hola mundo</h1>
     </div>
   );
 }
